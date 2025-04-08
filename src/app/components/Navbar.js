366082cf3f93cf08a17437c419bb50c58bd200cc
enzo antilipi <enzoantilipi1@gmail.com>
2025-04-08 13:13:17 -0300

    actualizaciones version mobile

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -2,12 +2,13 @@
 
 import styles from '../Styles/Navbar.module.css'
 import { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Link from "next/link"
 
 
 export default function Navbar(){
  const [scrolling, setScrolling] = useState(false);
-
+ const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       setScrolling(window.scrollY > 50);
@@ -17,11 +18,15 @@ export default function Navbar(){
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const toggleMenu = () => setIsOpen(!isOpen);
     return(
       <nav  className={`${styles.navbar} ${scrolling ? styles.scrolled : ""}`}>
         <div className={styles.navContainer}>
             <h1 className={styles.logo}>Pixel&Code</h1>
-            <ul className={styles.navLinks}>
+               <button className={styles.menuToggle} onClick={toggleMenu}>
+          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+            <ul className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
             <Link className={styles.navLink} href="/">Inicio</Link>
             <Link className={styles.navLink} href="/about">Quienes somos</Link>
             <Link className={styles.navLink} href="/proyects">Proyectos</Link>
