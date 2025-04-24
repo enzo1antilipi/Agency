81bbdf046cd5c03cd4f956c70b6fe669560d7abd
enzo antilipi <enzoantilipi1@gmail.com>
2025-04-24 19:06:12 -0300

    agregue logo

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -4,8 +4,13 @@ import styles from '../Styles/Navbar.module.css'
 import { useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Link from "next/link"
+import { Inter } from "next/font/google"
 
 
+const inter = Inter({
+  subsets: ["latin"], // Idiomas compatibles
+  weight: "500", // Peso disponible para Italianno
+});
 export default function Navbar(){
  const [scrolling, setScrolling] = useState(false);
  const [isOpen, setIsOpen] = useState(false);
@@ -22,7 +27,8 @@ export default function Navbar(){
     return(
       <nav  className={`${styles.navbar} ${scrolling ? styles.scrolled : ""}`}>
         <div className={styles.navContainer}>
-            <h1 className={styles.logo}>Pixel&Code</h1>
+          
+              <img src="/Logo3.png" alt="Proyecto 1" className={styles.projectImage} />
                <button className={styles.menuToggle} onClick={toggleMenu}>
           {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
         </button>
